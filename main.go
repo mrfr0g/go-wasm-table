05a858b987cb68f1af7bbb9a9531a889187e92d3
Michael Camden <camden.michael@gmail.com>
2018-09-09 22:03:54 -0400

    more data

--- main.go
+++ main.go
@@ -6,11 +6,11 @@ import (
 
 var columns = []sisyphus.Column{
 	sisyphus.Column{Property: "name"},
-	sisyphus.Column{Property: "date"},
+	sisyphus.Column{Property: "email"},
 	sisyphus.Column{Property: "count"},
 }
 
 func main() {
-	table := sisyphus.Table{Total: 100, Columns: columns, CellHeight: 50, PageSize: 25}
+	table := sisyphus.Table{Total: 500, Columns: columns, CellHeight: 50, PageSize: 25}
 	table.Mount()
 }
